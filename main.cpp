bd722cf6d4a84c5283ad3d0fc81012a4b0f2cceb
Plomkar <narikmaxet227@gmail.com>
2026-06-11 16:33:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@ using namespace std;
 
 enum class MenuOption {
     Exit = 0,
-    Task1 = 1,
-    Task2And3 = 2,
-    Task4 = 3,
-    Task6 = 4
+    powerFermat = 1,
+    gcdandInverse = 2,
+    elgamal = 3
 };
 
 int main() {
@@ -25,7 +24,6 @@ int main() {
         cout << "1. Задание 1. Возведение в степень по модулю (Ферма и Логарифм)" << endl;
         cout << "2. Задание 2 и 3. Расширенный алгоритм Евклида и инверсия по модулю" << endl;
         cout << "3. Задание 4. Криптосистема Эль-Гамаля (Шифрование файлов)" << endl;
-        cout << "4. Задание 6. Цепные дроби" << endl;
         cout << "0. Выход из программы" << endl;
         cout << "--------------------------------------------------" << endl;
         cout << "Выберите пункт меню: ";
@@ -42,15 +40,14 @@ int main() {
         MenuOption option = static_cast<MenuOption>(choice);
 
         if (option == MenuOption::Exit) {
-            cout << "Выход из программы. До свидания!" << endl;
+            cout << "Выход из программы" << endl;
             break;
         }
 
         cout << endl;
 
         switch (option) {
-            case MenuOption::Task1: {
-                // ЗАДАНИЕ 1: Возведение в степень
+            case MenuOption::powerFermat: {
                 cout << "ЗАДАНИЕ 1. Возведение в степень по модулю (a^x mod p) <<<" << endl;
                 int base, exp, mod;
                 
@@ -77,10 +74,10 @@ int main() {
                 int resLog = powerLog(base, exp, mod);
                 cout << "Результат: " << base << "^" << exp << " mod " << mod << " = " << resLog << endl;
                 break;
+            
             }
 
-            case MenuOption::Task2And3: {
-                // ЗАДАНИЕ 2 и 3: Алгоритм Евклида
+            case MenuOption::gcdandInverse: {
                 cout << "ЗАДАНИЕ 2 и 3. Расширенный алгоритм Евклида <<<" << endl;
                 int c_val, m_val;
                 
@@ -100,8 +97,7 @@ int main() {
                 break;
             }
 
-            case MenuOption::Task4: {
-                // ЗАДАНИЕ 4: Вариант 3 (Эль-Гамаль)
+            case MenuOption::elgamal: {
                 cout << "ЗАДАНИЕ 4. Шифрование файлов по схеме Эль-Гамаля <<<" << endl;
                 
                 // Имена файлов для работы
@@ -126,19 +122,83 @@ int main() {
                 int p_el, g_el, x_el, k_el;
                 
                 cout << "--- Настройка криптосистемы Эль-Гамаля ---" << endl;
+                
+                // 1. Валидация модуля p
                 while (true) {
-                    cout << "Введите простое число p (модуль, рекомендуемое > 255): ";
+                    cout << "Введите простое число p (модуль, необходимо > 255): ";
                     cin >> p_el;
-                    if (isPrime(p_el)) break;
-                    cout << "Ошибка! Модуль должен быть простым числом." << endl;
+                    if (cin.fail()) {
+                        cin.clear(); cin.ignore(32767, '\n');
+                        cout << "Ошибка! Введите целое число." << endl;
+                        continue;
+                    }
+                    if (p_el <= 255) {
+                        cout << "Ошибка! Модуль должен быть > 255 для побайтового шифрования." << endl;
+                        continue;
+                    }
+                    if (!isPrime(p_el)) {
+                        cout << "Ошибка! Число " << p_el << " не является простым." << endl;
+                        continue;
+                    }
+                    break; // Если всё ок, выходим из цикла
                 }
                 
-                cout << "Введите первообразный корень g (база): ";
-                cin >> g_el;
-                cout << "Введите ваш секретный ключ x (x < p): ";
-                cin >> x_el;
-                cout << "Введите случайный сессионный ключ k (взаимно простой с p-1): ";
-                cin >> k_el;
+                // 2. Валидация базы g
+                while (true) {
+                    cout << "Введите первообразный корень g (1 < g < " << p_el << "): ";
+                    cin >> g_el;
+                    if (cin.fail()) {
+                        cin.clear(); cin.ignore(32767, '\n');
+                        cout << "Ошибка! Введите целое число." << endl;
+                        continue;
+                    }
+                    if (g_el <= 1 || g_el >= p_el) {
+                        cout << "Ошибка! Число g должно строго лежать в диапазоне от 2 до " << p_el - 1 << "." << endl;
+                        continue;
+                    }
+                    break;
+                }
+
+                // 3. Валидация секретного ключа x
+                while (true) {
+                    cout << "Введите секретный ключ x (1 < x < " << p_el - 1 << "): ";
+                    cin >> x_el;
+                    if (cin.fail()) {
+                        cin.clear(); cin.ignore(32767, '\n');
+                        cout << "Ошибка! Введите целое число." << endl;
+                        continue;
+                    }
+                    if (x_el <= 1 || x_el >= p_el - 1) {
+                        cout << "Ошибка! Число x должно строго лежать в диапазоне от 2 до " << p_el - 2 << "." << endl;
+                        continue;
+                    }
+                    break;
+                }
+
+                // 4. Валидация сессионного ключа k
+                while (true) {
+                    cout << "Введите сессионный ключ k (1 < k < " << p_el - 1 << ", взаимно простой с " << p_el - 1 << "): ";
+                    cin >> k_el;
+                    if (cin.fail()) {
+                        cin.clear(); cin.ignore(32767, '\n');
+                        cout << "Ошибка! Введите целое число." << endl;
+                        continue;
+                    }
+                    if (k_el <= 1 || k_el >= p_el - 1) {
+                        cout << "Ошибка! Число k должно строго лежать в диапазоне от 2 до " << p_el - 2 << "." << endl;
+                        continue;
+                    }
+                    
+                    // Проверка на взаимную простоту k и (p-1)
+                    int dummy_u, dummy_v;
+                    // Используем скрытый вывод НОД, чтобы не спамить в консоль, передав фиктивные переменные
+                    int gcd_check = extendedgcd(p_el - 1, k_el, dummy_u, dummy_v);
+                    if (gcd_check != 1) {
+                        cout << "Ошибка! k=" << k_el << " и (p-1)=" << p_el - 1 << " не взаимно простые (НОД = " << gcd_check << "). Попробуйте другое k." << endl;
+                        continue;
+                    }
+                    break;
+                }
                 cout << endl;
 
                 // Шаг 4.3: Генерация открытого ключа
@@ -162,13 +222,6 @@ int main() {
                 getline(inDec, decryptedText);
                 cout << "[Результат] Считывание из '" << decryptedFile << "':" << endl;
                 cout << "Полученный текст: \"" << decryptedText << "\"" << endl;
-                break;
-            }
-
-            case MenuOption::Task6: {
-                // ЗАДАНИЕ 6: Цепные дроби
-                cout << "ЗАДАНИЕ 6. Разложение в цепную дробь и проверка уравнения <<<" << endl;
-                fraction(1256, 847);
                 break;
             }
 
