18f1cc71bc3172d9259cfe9826fca0ebbce2a351
Plomkar <narikmaxet227@gmail.com>
2026-06-11 16:32:27 +0700

    Update crypto_math.h

--- crypto_math.h
+++ crypto_math.h
@@ -1,6 +1,5 @@
 #ifndef CRYPTO_MATH_H
 #define CRYPTO_MATH_H
-#include <vector>
 
 // Проверка числа на простоту (для проверки модуля)
 bool isPrime(int n);
@@ -17,8 +16,5 @@ int extendedgcd(int a, int b, int &u, int &v);
 
 // Нахождение взаимообратного числа c^(-1) mod m
 int modInverse(int c, int m);
-
-// Задание 6. Вариант 3 (Разложение в цепную дробь и проверка уравнения)
-void fraction(int num, int den);
 
 #endif
